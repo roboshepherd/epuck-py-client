d9b69251eb24df9489909dba0bec02d6cb7ccce6
Md Omar Faruque Sarker <Mdomarfaruque.Sarker@newport.ac.uk>
2009-12-24 13:23:02 +0000

    Saving pose to data manager WIP

--- eric-prj/src/dbus_comm_handler.py
+++ eric-prj/src/dbus_comm_handler.py
@@ -2,9 +2,25 @@ import time, os, sys, sched, subprocess, re, signal, traceback
 import gobject, dbus, dbus.service, dbus.mainloop.glib 
 import multiprocessing,  logging
 from RILSetup import DBUS_IFACE,  DBUS_PATH
+from data_manager import *
+
+#datamgr_proxy = DataManager()
+iter = 0
+
+def save_pose(key,  pose_val):
+    global datamgr_proxy
+    try:
+        datamgr_proxy.mRobotPose[key] = pose_val
+        print datamgr_proxy.mRobotPose
+    except:
+       print "Err"
+       sys.exit(0) 
 
 def robot_signal_handler( sig,  val):
+    global iter
     print "Caught signal  %s (in robot signal handler) %d "  %(sig,  val)
+    iter = iter+1
+    save_pose(iter,  val)
  
 
 def main_loop():
@@ -17,13 +33,13 @@ def main_loop():
         sys.exit(0)
 
 def client_main(data_mgr,  dbus_iface= DBUS_IFACE,  dbus_path = DBUS_PATH,  sig = "RobotState"):
-     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-     bus = dbus.SessionBus()
-     try:
-        #bus_object = bus.get_object(dbus_iface, dbus_path)
-        #bus_object.connect_to_signal(sig, robot_signal_handler, dbus_interface=dbus_iface)
-        bus.add_signal_receiver(robot_signal_handler, dbus_interface = DBUS_IFACE, signal_name = sig)
-        main_loop()
-     except dbus.DBusException:
-        traceback.print_exc()
-        sys.exit(1)
+        global datamgr_proxy
+        datamgr_proxy = data_mgr
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        bus = dbus.SessionBus()
+        try:
+            bus.add_signal_receiver(robot_signal_handler, dbus_interface = DBUS_IFACE, signal_name = sig)
+            main_loop()
+        except dbus.DBusException:
+            traceback.print_exc()
+            sys.exit(1)
